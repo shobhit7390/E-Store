810811fa975b3d8364c93cac9af79202011f6d86
shobhit7390 <vms6402@gmail.com>
2023-05-18 14:56:35 +0530

    updateItem view and Creating csrf token

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import *
 
 # Create your views here.
@@ -33,4 +34,8 @@ def checkout(request):
 
     context={'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+    return JsonResponse('Item was added', safe=False)
 
