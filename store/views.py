d09865c205e3edded1b1d7f11c2f462a0635186c
shobhit7390 <vms6402@gmail.com>
2023-05-23 15:50:09 +0530

    Create order/orderItem

--- store/views.py
+++ store/views.py
@@ -44,5 +44,22 @@ def updateItem(request):
 
     print('Action:', action)
     print('productId:', productId)
+
+    customer=request.user.customer
+    product=Product.objects.get(id=productId)
+    order, created=Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created=OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity=(orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity=(orderItem.quantity - 1)
+    
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
     return JsonResponse('Item was added', safe=False)
 
